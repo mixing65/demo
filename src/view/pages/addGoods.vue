40db7399855e1eccf4afd9ac7e4d3d1131cd65c6
dapangzizz <1031298704@qq.com>
2020-09-01 21:16:45 +0800

    添加商品

--- src/view/pages/addGoods.vue
+++ src/view/pages/addGoods.vue
@@ -4,47 +4,28 @@
       <el-breadcrumb separator="/">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
-        <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+        <el-breadcrumb-item><a>商品列表</a></el-breadcrumb-item>
+        <el-breadcrumb-item>添加商品</el-breadcrumb-item>
       </el-breadcrumb>
       <!-- 内容区 -->
-      <div class="contentBox">
-        <el-input placeholder="请输入内容" v-model="queryObj.query" clearable @clear="toSearch">
-          <el-button slot="append" icon="el-icon-search" @click="toSearch"></el-button>
-        </el-input>
-        <el-button type="primary" @click="addGoods">添加商品</el-button>
-        <!-- 表格 -->
-        <el-table :data="goodsData" style="width: 100%">
-          <el-table-column type="index" label="#">
-          </el-table-column>
-          <el-table-column prop="goods_name" label="商品名称">
-          </el-table-column>
-          <el-table-column prop="goods_price" label="商品价格（元）">
-          </el-table-column>
-          <el-table-column prop="goods_weight" label="商品重量">
-          </el-table-column>
-          <el-table-column prop="add_time" label="创建时间">
-            <template slot-scope="scope">
-              {{scope.row.add_time | dateFormat}}
-            </template>
-          </el-table-column>
-          <el-table-column prop="address" label="操作" width="200">
-            <template v-slot:default="scope">
-              <el-button type="primary" icon="el-icon-edit" size="mini" @click="editInfo(scope.row.id)"></el-button>
-              <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteInfo(scope.row.goods_id)"></el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-        <!-- 分页 -->
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="queryObj.pagenum"
-          :page-sizes="[10, 20, 30, 40]"
-          :page-size="queryObj.pagesize"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="pagination.totals">
-        </el-pagination>
-      </div>
+        <!-- 顶部提示 -->
+        <el-alert
+          title="添加商品信息！" type="info" center show-icon>
+        </el-alert>
+        <!-- 顶部进度条 -->
+        <el-steps :space="200" :active="active" finish-status="success">
+          <el-step title="已完成"></el-step>
+          <el-step title="进行中"></el-step>
+          <el-step title="步骤 3"></el-step>
+        </el-steps>
+        <!-- 侧边导航 -->
+        <el-tabs tab-position="left" style="height: 200px;">
+          <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        </el-tabs>
+
     </div>
 </template>
 <script>
@@ -53,18 +34,7 @@ export default {
   data () {
     return {
       // 查询条件
-      searchInfo: '',
-      queryObj: {
-        query: '',
-        pagenum: 1,
-        pagesize: 10
-      },
-      // 表格数据
-      goodsData: [],
-      // 分页
-      pagination: {
-        totals: 10
-      }
+      active: 1
 
     }
   },
@@ -103,7 +73,7 @@ export default {
     },
     // 添加商品
     addGoods () {
-      this.$router.push('/home/addGoods')
+      this.$router.push('/goods/addGoods')
     },
     // 表格编辑
     editInfo () {
@@ -163,11 +133,8 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-.contentBox {
-  margin-top: 20px;
-  .el-input {
-    width: 50%;
+  /deep/ .el-alert--info.is-light {
+    margin-top: 20px;
   }
-}
 
 </style>
