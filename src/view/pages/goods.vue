c4a3affeb5f173fbde5d741ba822d8eeded8a219
zhengsai <1031298704.com>
2021-03-23 10:04:34 +0800

    goods中样式修改

--- src/view/pages/goods.vue
+++ src/view/pages/goods.vue
@@ -1,51 +1,76 @@
 <template>
-    <div>
-      <!-- 头部面包屑 -->
-      <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
-        <el-breadcrumb-item>商品列表</el-breadcrumb-item>
-      </el-breadcrumb>
-      <!-- 内容区 -->
-      <div class="contentBox">
-        <el-input placeholder="请输入内容" v-model="queryObj.query" clearable @clear="toSearch">
-          <el-button slot="append" icon="el-icon-search" @click="toSearch"></el-button>
-        </el-input>
-        <el-button type="primary" @click="addGoods">添加商品</el-button>
-        <!-- 表格 -->
-        <el-table :data="goodsData" style="width: 100%">
-          <el-table-column type="index" label="#">
-          </el-table-column>
-          <el-table-column prop="goods_name" label="商品名称">
-          </el-table-column>
-          <el-table-column prop="goods_price" label="商品价格（元）">
-          </el-table-column>
-          <el-table-column prop="goods_weight" label="商品重量">
-          </el-table-column>
-          <el-table-column prop="add_time" label="创建时间">
-            <template slot-scope="scope">
-              {{scope.row.add_time | dateFormat}}
-            </template>
-          </el-table-column>
-          <el-table-column prop="address" label="操作" width="200">
-            <template v-slot:default="scope">
-              <el-button type="primary" icon="el-icon-edit" size="mini" @click="editInfo(scope.row.id)"></el-button>
-              <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteInfo(scope.row.goods_id)"></el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-        <!-- 分页 -->
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="queryObj.pagenum"
-          :page-sizes="[10, 20, 30, 40]"
-          :page-size="queryObj.pagesize"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="pagination.totals">
-        </el-pagination>
-      </div>
+  <div>
+    <!-- 头部面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">商品管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 内容区 -->
+    <div class="contentBox">
+      <el-input placeholder="请输入内容"
+                v-model="queryObj.query"
+                clearable
+                @clear="toSearch">
+        <el-button slot="append"
+                   icon="el-icon-search"
+                   @click="toSearch"></el-button>
+      </el-input>
+      <el-button type="primary"
+                 @click="addGoods">添加商品</el-button>
+      <!-- 表格 -->
+      <el-table :data="goodsData"
+                style="width: 100%">
+        <el-table-column type="index"
+                         label="#"
+                         align="center">
+        </el-table-column>
+        <el-table-column prop="goods_name"
+                         label="商品名称"
+                         align="center">
+        </el-table-column>
+        <el-table-column prop="goods_price"
+                         label="商品价格（元）"
+                         align="center">
+        </el-table-column>
+        <el-table-column prop="goods_weight"
+                         label="商品重量"
+                         align="center">
+        </el-table-column>
+        <el-table-column prop="add_time"
+                         label="创建时间"
+                         align="center">
+          <template slot-scope="scope">
+            {{scope.row.add_time | dateFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column prop="address"
+                         label="操作"
+                         align="center"
+                         min-width="200">
+          <template v-slot:default="scope">
+            <el-button type="primary"
+                       icon="el-icon-edit"
+                       size="mini"
+                       @click="editInfo(scope.row.id)"></el-button>
+            <el-button type="danger"
+                       icon="el-icon-delete"
+                       size="mini"
+                       @click="deleteInfo(scope.row.goods_id)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination @size-change="handleSizeChange"
+                     @current-change="handleCurrentChange"
+                     :current-page="queryObj.pagenum"
+                     :page-sizes="[10, 20, 30, 40]"
+                     :page-size="queryObj.pagesize"
+                     layout="total, sizes, prev, pager, next, jumper"
+                     :total="pagination.totals">
+      </el-pagination>
     </div>
+  </div>
 </template>
 <script>
 export default {
@@ -87,7 +112,7 @@ export default {
   },
   methods: {
     getGoodsList () {
-      this.$http.get('goods', {params: this.queryObj})
+      this.$http.get('goods', { params: this.queryObj })
         .then(res => {
           console.log('goods---', res.data.data.total)
           this.goodsData = res.data.data.goods
@@ -169,5 +194,4 @@ export default {
     width: 50%;
   }
 }
-
 </style>
